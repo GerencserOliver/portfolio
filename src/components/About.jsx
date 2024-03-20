17195791c372a0c7ca81bdb4faae0bd9bea5c9a4
GerencserOliver <gerencser.oliver@students.jedlik.eu>
2024-03-20 21:58:14 +0100

    about part done, responsive

---
+++ src/components/About.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import UIUX from "../images/ui_ux.png";
+import '../styles/About.css';
+
+function About() {
+
+    return (
+        <>
+            <section className='about'>
+                <div className='about-img'>
+                    <img src={UIUX} alt="" />
+                </div>
+                <div className="about-text">
+                    <h1>About me</h1>
+                    <h2>Front-end Developer based in Győr, Hungary 📍</h2>
+                    <p>Hello there, I'm Oliver, a Frontend Developer specializing in user-centric design. I'm deeply passionate about crafting intuitive UI/UX experiences that resonate with users. <br/><br/> In my toolkit, React is my go-to framework, enabling me to build dynamic and responsive web applications. Combined with Tailwind CSS and TypeScript, I'm equipped to create polished and efficient solutions for various projects.</p>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default About;
