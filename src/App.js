46284b010d3199228c4d01e3d6f191a4147ee9e4
GerencserOliver <gerencser.oliver@students.jedlik.eu>
2024-03-25 21:31:23 +0100

    Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Route, Routes } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
-import { Route, Routes } from "react-router-dom";
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </>
   );
 }
